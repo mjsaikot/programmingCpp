a7068c4f8b8cbfdb6e2df2f51f9e0d6ba915ba86
Zobaer Islam <110193717+mjsaikot@users.noreply.github.com>
2023-07-30 16:35:35 +0600

    Module 5 update
    In Module 5 I have learned about string built-in function

---
+++ Module 5/Iterator.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+   string s;
+//    string::iterator it;
+   cin >> s;
+   for (auto it = s.begin(); it < s.end(); it++) {
+      cout << * it << endl;
+   }
+   //   cout<<*s.begin()<<endl;
+   //   cout<<*s.end()<<endl;
+   // //   for (int i = 0; i < s.size(); i++)
+   // //   {
+   // //     cout<<s[i]<<endl;
+   // //   }
+
+   return 0;
+}
---
+++ Module 5/string_input.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+   int x;
+   cin >> x;
+   // cin.ignore();
+   getchar();
+   string s;
+   getline(cin, s);
+   cout << x << endl;
+   cout << s << endl;
+   return 0;
+}
