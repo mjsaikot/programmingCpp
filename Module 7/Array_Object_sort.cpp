53bfec8630cc822bb60730f0c287ec22dca33a44
mjsaikot <mjsaikot@gmail.com>
2023-08-04 02:28:34 +0600

    Update Module

---
+++ Module 7/Array_Object_sort.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class student 
+{
+   public: string name;
+   int roll;
+   int marks;
+};
+int main() 
+{
+   //Array Object sorting by selection sort;; 
+   student a[2];
+   for (int i = 0; i < 2; i++) 
+   {
+      getline(cin, a[i].name);
+      cin >> a[i].roll >> a[i].marks;
+      getchar();
+   }
+
+   for (int i = 0; i < 1; i++) 
+   {
+      for (int j = i + 1; j < 2; j++) 
+	{
+         if (a[i].marks > a[j].marks) 
+	   {
+            swap(a[i], a[j]);
+         }
+      }
+   }
+   for (int i = 0; i < 2; i++) 
+   {
+      cout << a[i].name << " " << a[i].roll << " " << a[i].marks << endl;
+   }
+
+   return 0;
+}
---
+++ Module 7/Max_Min_of_Object.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class student {
+   public: string name;
+   int roll;
+   int marks;
+};
+int main() {
+   //Print the minimum and maximum marks of student;; 
+   student a[2];
+   for (int i = 0; i < 2; i++) {
+	  getline(cin, a[i].name);
+	  cin >> a[i].roll >> a[i].marks;
+	  cin.ignore();
+   }
+   student mx;
+   mx.marks = INT_MIN;
+   for (int i = 0; i < 2; i++)
+   {
+	  if(a[i].marks > mx.marks)
+	  {
+		 mx = a[i];
+	  }
+   }
+   cout<<mx.name<<" "<<mx.roll<<" "<<mx.marks<<endl;
+   student mn;
+   mn.marks = INT_MAX;
+   for (int i = 0; i < 2; i++)
+   {
+	  if(a[i].marks < mn.marks)
+	  {
+		 mn = a[i];
+	  }
+   }
+   cout<<mn.name<<" "<<mn.roll<<" "<<mn.marks<<endl;
+   
+   return 0;
+}
